ff91ea1ef109a4e92a00a424f1e37bb262825ee6
Blazej Bielski <bbielski@lyck.pl>
2022-11-27 14:16:04 +0100

    model todolist add

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
 class Todo(models.Model):
-    id = models
-
-
-id (int, identyfikator), title (str, tytuł
-zadania), done (bool, czy zadanie zostało zrobione), author_ip (str, adres ip osoby
-tworzącej), created_date (datetime, data utworzenia), done_date (datetime, data
-zakończenia zadania
+    id = models.IntegerField()
+    title = models.CharField()
+    done = models.BooleanField()
+    author_ip = models.CharField()
+    create_date = models.DateTimeField()
+    done_date = models.DateTimeField()
